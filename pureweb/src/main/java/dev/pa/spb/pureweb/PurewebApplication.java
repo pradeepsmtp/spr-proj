bf411738dc664cdb92ed683d74e612501c9ffb81
pradeepsmtp <pradeepsmtp@gmail.com>
2024-03-10 14:16:33 +0000

    pureweb sub proj added

---
+++ pureweb/src/main/java/dev/pa/spb/pureweb/PurewebApplication.java
@@ -0,0 +1,13 @@
+package dev.pa.spb.pureweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurewebApplication.class, args);
+	}
+
+}
---
+++ pureweb/src/test/java/dev/pa/spb/pureweb/PurewebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pa.spb.pureweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
