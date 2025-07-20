92552ef6ee1fd8e4c911101875b8604edb82b8a4
pkan <vazhadev@gmail.com>
2025-07-20 13:45:15 -0400

    sspring 3.5 level poj

---
+++ sb353/src/main/java/dev/pa/sb353/Sb353Application.java
@@ -0,0 +1,13 @@
+package dev.pa.sb353;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb353Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb353Application.class, args);
+	}
+
+}
---
+++ sb353/src/test/java/dev/pa/sb353/Sb353ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pa.sb353;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb353ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
